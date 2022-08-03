e21a3289f6806d087b55e11c4c30e1a1db529c18
Jose Chaves <josemcm85@gmail.com>
2022-08-02 23:36:38 -0600

    latest commit

--- obj/Release/net5.0-windows/MainWindow.g.i.cs
+++ obj/Release/net5.0-windows/MainWindow.g.i.cs
@@ -135,7 +135,7 @@ namespace TodayFocus {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TodayFocus;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TodayFocus;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
